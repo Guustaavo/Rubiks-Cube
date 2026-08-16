541cc4e380b85c5151b1665ef9722f70fb2d1c19
Gustavo <gustavo.guimaraes2148@gmail.com>
2026-08-16 12:32:00 -0300

    movimentos R

--- src/cuboMagico.js
+++ src/cuboMagico.js
@@ -56,9 +56,9 @@ function desenhar() {
 }
 
 function girarF() {
-    var temp0 = cube.up[2][0]
-    var temp1 = cube.up[2][1]
-    var temp2 = cube.up[2][2]
+    var temp0 = cube.up[2][0];
+    var temp1 = cube.up[2][1];
+    var temp2 = cube.up[2][2];
 
     cube.up[2][0] = cube.left[2][2]; cube.up[2][1] = cube.left[1][2]; cube.up[2][2] = cube.left[0][2];
     cube.left[0][2] = cube.down[0][0]; cube.left[1][2] = cube.down[0][1]; cube.left[2][2] = cube.down[0][2];
@@ -67,25 +67,55 @@ function girarF() {
 }
 
 function girarFInvertido() {
-    var temp0 = cube.up[2][0]
-    var temp1 = cube.up[2][1]
-    var temp2 = cube.up[2][2]
+    var temp0 = cube.up[2][0];
+    var temp1 = cube.up[2][1];
+    var temp2 = cube.up[2][2];
 
     cube.up[2][0] = cube.right[0][0]; cube.up[2][1] = cube.right[1][0]; cube.up[2][2] = cube.right[2][0];
     cube.right[0][0] = cube.down[0][2]; cube.right[1][0] = cube.down[0][1]; cube.right[2][0] = cube.down[0][0];
-    cube.down[0][0] = cube.left[0][2]; cube.down[0][1] = cube.left[2][2]; cube.down[0][2] = cube.left[2][2];
+    cube.down[0][0] = cube.left[0][2]; cube.down[0][1] = cube.left[1][2]; cube.down[0][2] = cube.left[2][2];
     cube.left[0][2] = temp2; cube.left[1][2] = temp1; cube.left[2][2] = temp0;
+}
+
+function girarR() {
+    var temp0 = cube.up[2][2];
+    var temp1 = cube.up[1][2];
+    var temp2 = cube.up[0][2];
+    
+    cube.up[2][2] = cube.front[2][2]; cube.up[1][2] = cube.front[1][2]; cube.up[0][2] = cube.front[0][2];
+    cube.front[0][2] = cube.down[0][2]; cube.front[1][2] = cube.down[1][2]; cube.front[2][2] = cube.down[2][2];
+    cube.down[0][2] = cube.back[2][0]; cube.down[1][2] = cube.back[1][0]; cube.down[2][2] = cube.back[0][0];
+    cube.back[0][0] = temp0; cube.back[1][0] = temp1; cube.back[2][0] = temp2;
+}
+
+function girarRInvertido() {
+    var temp0 = cube.up[2][2];
+    var temp1 = cube.up[1][2];
+    var temp2 = cube.up[0][2];
+    
+    cube.up[2][2] = cube.back[0][0]; cube.up[1][2] = cube.back[1][0]; cube.up[0][2] = cube.back[2][0];
+    cube.back[0][0] = cube.down[2][2]; cube.back[1][0] = cube.down[1][2]; cube.back[2][0] = cube.down[0][2];
+    cube.down[0][2] = cube.front[0][2]; cube.down[1][2] = cube.front[1][2]; cube.down[2][2] = cube.front[2][2];
+    cube.front[0][2] = temp2; cube.front[1][2] = temp1; cube.front[2][2] = temp0;
 }
 
 function jogo() {
     desenhar();
-    var movimento = leia.keyInSelect(["F", "F'"])
+    var movimento = leia.keyInSelect(["F", "F'", "R", "R'"])
     if (movimento === 0) {
         girarF();
         jogo();
     }
     else if (movimento === 1) {
         girarFInvertido();
+        jogo();
+    }
+    else if(movimento === 2) {
+        girarR();
+        jogo();
+    }
+    else if(movimento === 3) {
+        girarRInvertido();
         jogo();
     }
 }
